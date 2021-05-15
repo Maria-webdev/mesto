e537021bc98549bca83b321693543fbb623afa35
Maria Averina <amaria.webdev@gmail.com>
2021-05-15 09:40:46 +0300

    исправить ошибки в сборке webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,13 @@
 
-const editButton = new URL('../images/profile/__adit-button/profile__adit-button.svg', import.meta.url);
-const deleteButton = new URL('../images/element/__delete-button/element__delete-button.svg', import.meta.url);
-const likeButton = new URL('../images/element/__like-button/element__like-button.svg', import.meta.url);
-const closeButton = new URL('../images/popup/__close-button/popup__close-button.svg', import.meta.url);
-const addButton = new URL('../images/__like-button/element__like-button.svg', import.meta.url);
-const likeButtonActive = new URL('../images/profile/__add-button/profile__add-button-center.svg', import.meta.url);
-const pic1 = new URL('../images/element/__pic/Aachen_Germany_Imperial-Cathedral.png', import.meta.url);
-const pic2 = new URL('../images/element/__pic/Gartenbrunnen_der_Würzburger_Residenz.png', import.meta.url);
-const pic3 = new URL('../images/element/__pic/Koelner_Dom.png', import.meta.url);
-const pic4 = new URL('../images/element/__pic/Schloss_Augustusburg_Bruehl.png', import.meta.url);
-const pic5 = new URL('../images/element/__pic/Speyer-Cathedral.png', import.meta.url);
-const pic6 = new URL('../images/element/__pic/Trier_Porta_Nigra_BW_3.png', import.meta.url);
 
-const iconList = [
-  // меняем исходные пути на переменные
-  { name: 'Edit Button', link: editButton },
-  { name: 'Delete Button', link: deleteButton },
-  { name: 'Like Button', link: likeButton },
-  { name: 'Close Button', link: closeButton },
-  { name: 'Add Button', link: addButton },
-  { name: 'Like Button Active', link: likeButtonActive },
-  { name: 'Аахен', link: pic1 },
-  { name: 'Вюрцбург', link: pic2 },
-  { name: 'Кёльн', link: pic3 },
-  { name: 'Аугсбург', link: pic4 },
-  { name: 'Шпейер', link: pic5 },
-  { name: 'Трир', link: pic6 },
-]; 
 
 import './index.css';
-import { initialCards } from "../scripts/initial-сards.js";
+import { initialCards } from "../utils/initial-сards.js";
 import { popupEdit, popupAdd, popupPic, popups, popup, popupFormEdit, popupFormAdd, closeEditBtn, closeAddBtn,
          closePicBtn, closeBtn, editBtn, addBtn, nameInput, aboutInput, nameForm, aboutForm, formEdit, formAdd,
-         elements, placeInput, linkInput, pic, picTitle, validationElements} from "../scripts/consts.js";
-import { Card } from "../scripts/Card.js";
-import { formValidator } from "./formValidator.js";
+         elements, placeInput, linkInput, pic, picTitle, validationElements} from "../utils/consts.js";
+import { Card } from "../components/Card.js";
+import { formValidator } from "../components/formValidator.js";
 
 const formEditValidator = new formValidator(validationElements, popupFormEdit);
 formEditValidator.enableValidation();
